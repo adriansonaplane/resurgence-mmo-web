9dedbb34b82f2798c3d53d3d471f48a2d634bd03
adriansonaplane <109830657+adriansonaplane@users.noreply.github.com>
2026-06-28 16:40:34 -0700

    Scaffold account service boundary

--- apps/api/src/modules/entitlements/entitlements.module.ts
+++ apps/api/src/modules/entitlements/entitlements.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AccountServiceModule } from '../account-service/account-service.module';
 import { EntitlementBridgeController, EntitlementsController } from './entitlements.controller';
 import { EntitlementsService } from './entitlements.service';
 
 @Module({
+  imports: [AccountServiceModule],
   controllers: [EntitlementsController, EntitlementBridgeController],
   providers: [EntitlementsService],
   exports: [EntitlementsService],
